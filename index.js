dc6a384362481b8f8cf2f593682eb79bd3ea798d
Tomasz Sikora <frakti7@gmail.com>
2016-03-31 02:56:59 +0200

    add voucherify integration, test validation

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const nfc = require('explorenfc')
+const voucherify = require('voucherify')
+const voucherifyClient = voucherify({
+    applicationId: "c70a6f00-cf91-4756-9df5-47628850002b",
+    clientSecretKey: "3266b9f8-e246-4f79-bdf0-833929b1380c"
+});
 
 const db = {
   '04C00DEAFC3880': 'piwo-RQ-vArGTfO'
@@ -9,9 +14,18 @@ const db = {
 function startListening () {
   nfc.init('/usr/bin/explorenfc-basic')
   nfc.read((nfcEvent) => {
-    if (nfcEvent){
+    if (nfcEvent) {
       console.log('id', nfcEvent.id)
       console.log('value', nfcEvent.value)
+
+      voucherifyClient.get(db[nfcEvent.id])
+        .then(function (result) {
+            console.log(result);
+        })
+        .catch(function (error) {
+            console.error("Error: %s", error);
+        });
+
     } else {
       console.log('no NFC Event')
     }
