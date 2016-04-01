13643250059e56204e19ed3aa2da8bafb859778e
Tomasz Sikora <frakti7@gmail.com>
2016-04-01 10:06:53 +0200

    use redis to store codes

--- proxy.js
+++ proxy.js
@@ -1,22 +1,33 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
-const expressWs = require('express-ws')(app)
+app.use(bodyParser.json())
+require('express-ws')(app)
 
+const Redis = require('ioredis');
+const redis = new Redis();
+
+// TOOD load vouchers via API
+redis.set('voucherify:code', 'test code');
+redis.set('voucherify:used', 0);
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 });
 
 app.post('/register', function(req, res) {
-  const {device} = req.body
+  const device = req.body.device
 
   if (!device) {
     return res.status(400).send({error: 'Missing device ID.'})
   }
 
-  res.json({
-    code: 'test code'
-  })
+  redis.get('voucherify:code', function (err, result) {
+    res.json({
+      code: result
+    })
+  });
+
 });
