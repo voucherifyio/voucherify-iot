0f09acdcce22221a2ca4e6d6f0399c97a351ae76
Lukasz Kulig <lukasz.kulig88@gmail.com>
2016-04-01 18:14:28 +0200

    Fixes

--- public/client.js
+++ public/client.js
@@ -16,18 +16,31 @@ $(document).ready(function () {
 
       const voucher = JSON.parse(evt.data)
 
-      if(voucher.code) {
+      if (voucher.code) {
         document.getElementById('beer').play();
         $('.shape')
           .shape('set next side', '.beer.side')
           .shape('flip right');
         $('#code-value').text(voucher.code);
+
+        setTimeout(function () {
+          $('.shape')
+            .shape('set next side', '.question.side')
+            .shape('flip right');
+        }, 5000)
       } else {
         document.getElementById('sad').play();
         $('.shape')
           .shape('set next side', '.sad.side')
           .shape('flip left');
+
         $('#code-value').text('');
+        setTimeout(function () {
+          $('.shape')
+            .shape('set next side', '.question.side')
+            .shape('flip right');
+        }, 5000)
+
       }
     };
 
